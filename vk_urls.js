80db0f374cf07155c1888eaef7cfd77acbfd861f
monkey <monkey@yandex-team.ru>
2016-05-03 18:42:57 +0300

    initial commit

---
+++ vk_urls.js
@@ -0,0 +1,1 @@
+[].forEach.call(document.getElementsByClassName("audio"), function (i) {i.addEventListener("click", function myFunction(){console.log(document.getElementById('audio_info' + i['id'].match(/[_0-9]+/)[0]).value)});})
