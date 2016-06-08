d85aaf55c28bf5dcfa43b40a6fe59474ae536036
Alexey Martishin <alex.martishin@yandex.ru>
2016-06-08 07:40:55 +0300

    Update jquery_override_angular.js

--- jquery_override_angular.js
+++ jquery_override_angular.js
@@ -12,7 +12,7 @@ function WaitForLoad(){
 
 function AddButton(){
     var ticket = window.location.href.split("/").slice(-2, -1);
-    $('.panel-footer').first().append('<button class="btn btn-default" id="autodeploy-btn">Autodeploy</button>');
+    $('.panel-footer').first().append('<button class="btn btn-default" id="autodeploy-btn">Autocommit</button>');
     $("#autodeploy-btn").click(function() {
         console.log('success');
         $.getJSON('https://foo.bar/?ticket=' + ticket, function(data) {});
@@ -20,6 +20,15 @@ function AddButton(){
 }
 
 (function() {
-    WaitForLoad();
-    found.done(AddButton);
+    if (window.location.href.search('/#/t/') != -1) {
+        WaitForLoad();
+        found.done(AddButton);
+    }
+    
+    window.onhashchange = function() {
+        if (window.location.href.search('/#/t/') != -1) {
+            WaitForLoad();
+            found.done(AddButton);
+        }
+    };
 })();
